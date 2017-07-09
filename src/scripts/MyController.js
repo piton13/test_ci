1f08b936ba6a30e8854831b470087f23702058e9
Igor Mykhaylyuk <igor13luk@gmail.com>
2017-07-09 14:06:47 +0300

    add cross domain request

--- src/scripts/MyController.js
+++ src/scripts/MyController.js
@@ -2,13 +2,22 @@ import angular from 'angular';
 
 export default class MyController {
 
-    constructor(inheritanceService) {
+    constructor(inheritanceService, $http) {
         'ngInject';
+        this.dataFromServer = '';
         this.inheritanceService = inheritanceService;
+        this.http = $http;
     }
 
     alertMessage() {
         console.log(this.inheritanceService.getSuperClass());
         alert('the name of superClass is:' + this.inheritanceService.getSuperClass().name);
+    }
+
+    showData() {
+        this.http.get('http://192.168.1.119:7776/word')
+            .then((response) => {
+                this.dataFromServer = response;
+            });
     }
 };
--- src/scripts/MyController.spec.js
+++ src/scripts/MyController.spec.js
@@ -2,15 +2,25 @@ import MyController from './MyController.js';
 
 describe('initial test', () => {
 	let sut;
+	let $http = {
+		get: env.stub().returns({
+			then: env.stub()
+		})
+	};
 
 	beforeEach(() => {
-		sut = new MyController;
+		sut = new MyController($http);
 	});
 	
 	it('should alert message when click onto appropriate button', function() {
 		window.alert = env.stub();
 		sut.alertMessage();
 		window.alert.should.called;
+	});
+
+	it('should make request to cross domain server', function() {
+		sut.showData();
+		$http.get.should.calledWith('http://192.168.1.119:7776/word');
 	});
 	
 });
